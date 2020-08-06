1d5a14584bf9cf82a7b6e9d411e2ca834c9200b2
Conor <conor.egan@uea.ac.uk>
2020-08-06 20:32:38 +0100

    Should now work on mac and linux, as well as windows

--- code/src/main/java/uk/co/conoregan/Main.java
+++ code/src/main/java/uk/co/conoregan/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application
 {
     public static void main(String[] args)
@@ -16,7 +18,7 @@ public class Main extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/rename.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("view/rename.fxml")));
         Scene scene = new Scene(root, 1280, 720);
 
         root.setId("pane");
---
+++ code/src/main/java/uk/co/conoregan/NewMain.java
@@ -0,0 +1,9 @@
+package uk.co.conoregan;
+
+public class NewMain
+{
+    public static void main(String[] args)
+    {
+        Main.main(args);
+    }
+}
