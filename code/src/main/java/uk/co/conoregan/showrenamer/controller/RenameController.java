3713d3cea2ece6edc1bad70b8dc7a53aa1f2c972
Conor Egan <68134729+c-eg@users.noreply.github.com>
2020-08-29 19:26:56 +0100

    Forgot to remove testing print statement.

--- code/src/main/java/uk/co/conoregan/showrenamer/controller/RenameController.java
+++ code/src/main/java/uk/co/conoregan/showrenamer/controller/RenameController.java
@@ -186,7 +186,6 @@ public class RenameController implements Initializable
     @FXML
     public void renameSelected()
     {
-        System.out.println(files);
 //        if (listRenameFrom.size() > 0)
 //        {
 //            String toRename = listViewRenameFrom.getSelectionModel().getSelectedItem();
