a4e7142fb5befc7d754884c361cf38dd2486206b
Conor Egan <68134729+c-eg@users.noreply.github.com>
2020-10-15 15:22:33 +0100

    Created search page fxml and controller files

---
+++ code/src/main/java/uk/co/conoregan/showrenamer/controller/SearchController.java
@@ -0,0 +1,25 @@
+package uk.co.conoregan.showrenamer.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SearchController implements Initializable
+{
+    // Menu buttons
+    @FXML
+    private Button buttonMenuRename;
+    @FXML
+    private Button buttonMenuSearch;
+    @FXML
+    private Button buttonMenuSettings;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+
+    }
+}
