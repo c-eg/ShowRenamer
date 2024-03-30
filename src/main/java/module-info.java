7e1e2770960dd3572c154369214af500db5b3e57
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-03-30 16:18:44 +0000

    progress so far

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,12 +8,4 @@ module uk.co.conoregan.showrenamer {
 
     opens uk.co.conoregan.showrenamer to javafx.fxml, javafx.graphics;
     opens uk.co.conoregan.showrenamer.controller to javafx.fxml;
-
-    exports uk.co.conoregan.showrenamer;
-    exports uk.co.conoregan.showrenamer.api;
-    exports uk.co.conoregan.showrenamer.config.preference;
-    exports uk.co.conoregan.showrenamer.config.property;
-    exports uk.co.conoregan.showrenamer.controller;
-    exports uk.co.conoregan.showrenamer.suggestion;
-    exports uk.co.conoregan.showrenamer.util;
 }
