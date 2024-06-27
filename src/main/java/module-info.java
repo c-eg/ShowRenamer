1057d676c2f73fd08c215277784bcbff67422035
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-06-27 15:12:21 +0100

    remove crypto package - it was added to wrapper library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module uk.co.conoregan.showrenamer {
     requires javafx.controls;
     requires org.apache.commons.io;
     requires org.slf4j;
-    requires jdk.crypto.cryptoki;
 
     opens uk.co.conoregan.showrenamer to javafx.fxml, javafx.graphics;
     opens uk.co.conoregan.showrenamer.controller to javafx.fxml;
